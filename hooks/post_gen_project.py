1800863fc4b66281207d2e45fb65f54daec0c8ab
Peter Rodríguez Casado <petrodcas@gmail.com>
2024-05-24 02:18:24 +0200

    Fix: Missing modules directory. Fix: Python code adapted to remove non-empty dirs.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,8 +4,8 @@ def get_all_conditional_removable_dirs(context):
   return [key for key, value in context.items() if key.endswith('_dir') and key.startswith('include_') and value is False]
 
 if __name__ == '__main__':
-  import os
+  import shutil
   context = {{cookiecutter}}
   for dir in get_all_conditional_removable_dirs(context):
     dir_path = dir.replace('_dir', '').replace('include_', '')
-    os.rmdir(dir_path)
+    shutil.rmtree(dir_path, ignore_errors=True)
